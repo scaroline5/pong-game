4a40c9ee1d84327d07dc600e31a1d11c6ef68d15
Suellen Caroline da Silva <sucaroline5@gmail.com>
2021-09-02 10:05:12 -0300

    Create the game screen

---
+++ main.py
@@ -0,0 +1,8 @@
+from turtle import Screen
+
+# Create the game Screen
+screen = Screen()
+screen.bgcolor("black")
+screen.title("Pong Game")
+screen.setup(width=800, height=600)
+screen.exitonclick()
