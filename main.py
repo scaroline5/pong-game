a8fc5baf61f675c05ea0bd3bd79380656966bd6a
Suellen Caroline da Silva <sucaroline5@gmail.com>
2021-09-02 11:09:41 -0300

    Create and move a paddle, then create another paddle

--- main.py
+++ main.py
@@ -1,8 +1,34 @@
-from turtle import Screen
+from turtle import Screen, Turtle
+from paddle import Paddle
 
 # Create the game Screen
 screen = Screen()
 screen.bgcolor("black")
 screen.title("Pong Game")
 screen.setup(width=800, height=600)
+# Turn animation off
+screen.tracer(0)
+
+# Create the paddle
+right_paddle = Paddle((350, 0))
+left_paddle = Paddle((-350, 0))
+
+# Listen to keyboard
+screen.listen()
+
+# Move Right Paddle
+screen.onkeypress(key="Up", fun=right_paddle.move_up)
+screen.onkeypress(key="Down", fun=right_paddle.move_down)
+
+# Move Left Paddle
+screen.onkeypress(key="w", fun=left_paddle.move_up)
+screen.onkeypress(key="s", fun=left_paddle.move_down)
+
+game_is_on = True
+while game_is_on:
+    # Refresh the screen to show elements after turning animation off
+    screen.update()
+
+
+# Keep the window open
 screen.exitonclick()
---
+++ paddle.py
@@ -0,0 +1,20 @@
+from turtle import Turtle
+
+
+class Paddle(Turtle):
+
+    def __init__(self, position):
+        super().__init__()
+        self.shape("square")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.color("white")
+        self.penup()
+        self.goto(position)
+
+    def move_up(self):
+        new_yposition = self.ycor() + 20
+        self.goto(self.xcor(), new_yposition)
+
+    def move_down(self):
+        new_yposition = self.ycor() - 20
+        self.goto(self.xcor(), new_yposition)
